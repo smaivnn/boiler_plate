e78e869267f2414b9a83c41a162f3119d0de947f
smaivnn <janus321@naver.com>
2023-03-26 15:58:14 +0900

    feat: client 삭제

--- client/src/components/Nav.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Nav = () => {
-  return <div>NAV</div>;
-};
-
-export default Nav;
--- client/src/pages/AllowedPage.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AllowedPage = () => {
-  return <div>AllowedPage</div>;
-};
-
-export default AllowedPage;
